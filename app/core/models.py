3c8cc47ae6da7fc798b38bb07ebd7c83318cd05e
MurilowJr <murilobrito.jr@gmail.com>
2024-09-25 20:19:05 -0300

    refactor: adjustment on table issues (participants, participant_authorized_third_parties, authorized_third_parties)

--- app/core/models.py
+++ app/core/models.py
@@ -21,23 +21,30 @@ class Paymasters(models.Model):
     updated_at = models.DateField(null=True)
 
 
-class Authorized_Third_Parties(models.Model):
-    id = models.AutoField(primary_key=True)
-
-
 class Participants(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     state = models.CharField(max_length=255)
     contact_phone_number = models.CharField(max_length=255)
     document_number = models.CharField(max_length=255)
-    authorized_third_party_id = models.ForeignKey(Authorized_Third_Parties,on_delete=models.CASCADE)
     company_name = models.CharField(max_length=255)
     paymaster_id = models.ForeignKey(Paymasters,on_delete=models.CASCADE)
 
 
+class Participant_Authorized_Third_Parties(models.Model):
+    id = models.AutoField(primary_key=True)
+    participant_id = models.ForeignKey(Participants, on_delete=models.CASCADE)
+    authorized_third_party_id = models.CharField(max_lenght=255,unique=True)
+    deleted_at = models.DateField(null=True,blank=True)
+    created_at = models.DateField(null=True)
+    updated_at = models.DateField(null=True)
+    state = models.CharField(max_length=255)
+    approved_at = models.DateField(null=True)
+    rejected_at = models.DateField(null=True)
+
+
 class Fk_Authorized_Third_Party_Participants(models.Model):
-    authorized_third_party_id = models.ForeignKey(Authorized_Third_Parties,on_delete=models.CASCADE)
+    authorized_third_party_id = models.ForeignKey(Participant_Authorized_Third_Parties,on_delete=models.CASCADE)
     participant_id = models.ForeignKey(Participants,on_delete=models.CASCADE)
 
     class Meta:
@@ -45,17 +52,6 @@ class Fk_Authorized_Third_Party_Participants(models.Model):
         verbose_name = 'Authorized Third Party Participant'
         verbose_name_plural = 'Authorized Third Party Participants'
 
-
-class Participant_Authorized_Third_Parties(models.Model):
-    id = models.AutoField(primary_key=True)
-    participant_id = models.ForeignKey(Participants, on_delete=models.CASCADE)
-    authorized_third_party_id = models.ForeignKey(Authorized_Third_Parties,on_delete=models.CASCADE)
-    deleted_at = models.DateField(null=True,blank=True)
-    created_at = models.DateField(null=True)
-    updated_at = models.DateField(null=True)
-    state = models.CharField(max_length=255)
-    approved_at = models.DateField(null=True)
-    rejected_at = models.DateField(null=True)
 
 class Asset_Trade_Bills(models.Model):
     id = models.AutoField(primary_key=True)
